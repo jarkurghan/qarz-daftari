67e96cc8c1d3ca80632684dd0e70f256226ab505
jarkurghan <jarkurghan@gmail.com>
2024-10-28 18:51:23 +0500

    add debt: initial value

--- components/Debt/CreateDebt.jsx
+++ components/Debt/CreateDebt.jsx
@@ -9,22 +9,35 @@ import * as Yup from "yup";
 const defaultSchema = Yup.object().shape({
     name: Yup.string().required("Matn kiriting!"),
 });
+const defaultValue = {
+    name: undefined,
+    amount: undefined,
+    date: undefined,
+    debt_type: undefined,
+};
 
 export default function CreateDebtPage() {
     const active = useSelector(getActiveJournal);
     const journalValidation = useSelector(getJournalValidation);
     const [validation, setValidation] = useState(defaultSchema);
+    const [initialValue, setInitialValue] = useState(defaultValue);
+    const [formaShakli, setFormaShakli] = useState(null);
 
     useEffect(() => {
         const currentJournalVal = journalValidation[active.name];
         const schema = { name: Yup.string().required("Matn kiriting!") };
+        const value = { name: "", amount: 0, date: "", debt_type: "" };
+        const formaShakli = new Set(["name", "date", "amount", "debt_type"]);
 
         if (currentJournalVal.folderable) {
             schema.folder = Yup.string().optional();
+            value.folder = "/";
+            formaShakli.add("folder");
         }
 
         if (currentJournalVal.debt_type_required) schema.debt_type = Yup.string().required("Qaytarish vaqtini kitiring!");
         else schema.debt_type = Yup.string().optional();
+        if (currentJournalVal.debt_type_default) value.debt_type = currentJournalVal.debt_type_default;
 
         if (currentJournalVal.amount_type === "float") {
             if (currentJournalVal.amount_required) {
@@ -47,16 +60,22 @@ export default function CreateDebtPage() {
         else schema.date = Yup.string().optional();
 
         if (currentJournalVal.addressable) {
+            formaShakli.add("address");
+            value.address = "";
             if (currentJournalVal.address_required) schema.address = Yup.string().required("Address kitiring!");
             else schema.address = Yup.string().optional();
         }
         if (currentJournalVal.phonable) {
+            formaShakli.add("phone");
+            value.phone = "";
             if (currentJournalVal.phone_required) schema.phone = Yup.string().required("Telefon raqam kitiring!");
             else schema.phone = Yup.string().optional();
         }
 
         const validationSchema = Yup.object().shape(schema);
         setValidation(validationSchema);
+        setInitialValue(value);
+        setFormaShakli(formaShakli);
     }, [active]);
 
     return (

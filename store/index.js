d2b06d1688e1f45a9247c6980e189bdc74f1f73a
jarkurghan <jarkurghan@gmail.com>
2024-11-13 14:16:37 +0500

    store

--- store/index.js
+++ store/index.js
@@ -1,9 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import alert from "./alert";
 import token from "./token";
-import filter from "./filter";
-import prevFilter from "./prevFilter";
-import timeClock from "./timeClock";
 import journal from "./journal";
 import visibleBottomNavigation from "./bottomnav";
 import activeJournal from "./activeJournal";
@@ -15,12 +12,9 @@ export const store = configureStore({
     reducer: {
         alert,
         token,
-        filter,
         getters,
-        prevFilter,
         journal,
         activeJournal,
-        timeClock,
         visibleBottomNavigation,
         isAuthenticatedUser,
         journalValidation,
